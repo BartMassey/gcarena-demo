f5af1ff953619d558cfb74bfc3cd9b3e1018db07
Bart Massey <bart.massey@gmail.com>
2022-12-21 19:32:48 -0800

    added documentation, cleaned up the .clone() mess

--- gcarena-demo.rs
+++ gcarena-demo.rs
@@ -1,12 +1,55 @@
+/*!
+This toy app is a demo of `gc-arena` by way of LISP-style
+lists.  A library could probably be built for this to good
+effect, but this is maybe a better demo of the low-level use
+of the `gc-arena` crate.
+
+Thanks much to Manish Goregaokar for
+[this article](https://manishearth.github.io/blog/2021/04/05/a-tour-of-safe-tracing-gc-designs-in-rust/)
+that contained a nice tiny example of the use of
+`gc-arena`. This was really helpful in understanding how to
+use this crate.
+*/
+
 use gc_arena::*;
 
+/// Start with a structure that will be treated as the
+/// garbage collector "root".
+///
+/// Anything that might be mutated needs a [GcCell] rather than
+/// a [Gc], so go with that.
+///
+/// Our root will be a `Vec` of top-level [List]s that the
+/// GC will keep as long as they are referenced.
+///
+/// You will almost always want `no_drop` when deriving [Collect].
 #[derive(Collect)]
 #[collect(no_drop)]
 struct MyRoot<'gc>(GcCell<'gc, Vec<List<'gc>>>);
 
+// This macro will define a `struct MyArena` that knows
+// about its roots.
 make_arena!(MyArena, MyRoot);
 
-#[derive(Collect)]
+/// The fundamental list data structure of LISP is the "cons
+/// cell".  A cons cell normally consists of a pair of references
+/// to values: the "car" and "cdr". For a list,
+/// the car will contain a list value and the cdr will reference
+/// the rest of the list. LISP cons cells are sometimes used for
+/// other things, though.
+///
+/// This definition thus is not the way things are normally
+/// done in LISP, inasmuch as `Nil` wouldn't be part of a
+/// normal definition and we are only doing homogenous lists
+/// of `u32`s which gives us a simplification. This way
+/// makes things easier in Rust.  I think.
+///
+/// (The terms car, cdr and cons are historical curiosities:
+/// "cons" is short for "construct", and "car" and "cdr"
+/// were the "contents of the address register" and
+/// "contents of the decrement register" on some ancient
+/// hardware.)
+#[derive(Debug, Collect, Clone, Copy)]
 #[collect(no_drop)]
 enum ConsCell<'gc> {
     Nil,
@@ -14,19 +57,49 @@ enum ConsCell<'gc> {
 }
 use ConsCell::*;
 
-#[derive(Collect)]
+/// The head of the [List] is wrapped so that the GC
+/// understands that it is collectible. This is just a
+/// convenience to avoid some really ugly types. There
+/// should be no impact on the actual data structure, since
+/// the compiler can elide the tag here.
+///
+/// Note that the `Copy` implementation here won't actually
+/// copy anything but a pointer. This is the beauty of GC.
+#[derive(Debug, Collect, Clone, Copy)]
 #[collect(no_drop)]
 struct List<'gc>(GcCell<'gc, ConsCell<'gc>>);
 
 impl<'gc> List<'gc> {
+    /// An empty [List] is just a pointer to a [Nil] cons cell
+    /// in memory.
+    ///
+    /// For this GC to be able to collect it, it has to be
+    /// aware of what arena it is in, etc. This is the role
+    /// of `mc`.
     fn new(mc: MutationContext<'gc, '_>) -> Self {
         List(GcCell::allocate(mc, Nil))
     }
 
+    /// To stick a value on the front of a [List], allocate
+    /// a new [ConsCell] with the value and the rest of the list.
+    ///
+    /// Note that this allows "tail sharing": if you cons
+    /// two different values onto the same list, you get two
+    /// lists that share every cons cell but their first.
+    /// Reference counting or garbage collection is needed
+    /// to make sure that freeing the tail of one list does
+    /// not free the other. Because LISPs typically allow mutating
+    /// the cdr arbitrarily, GC is needed to deal with cycles
+    /// that may result.
     fn cons(self, mc: MutationContext<'gc, '_>, car: u32) -> Self {
         List(GcCell::allocate(mc, Cons { car, cdr: self }))
     }
 
+    /// Get the value at the head of the list, or panic if
+    /// the list is empty.
+    ///
+    /// It would be more Rustic to return an `Option<u32>`,
+    /// but this way is LISP tradition.
     fn car(&self) -> u32 {
         if let Cons { car, .. } = *self.0.read() {
             car
@@ -35,48 +108,60 @@ impl<'gc> List<'gc> {
         }
     }
 
+    /// Get the "rest" of the list after the first element,
+    /// or panic if the list is empty.
+    ///
+    /// It would be more Rustic to return an `Option<List>`,
+    /// but this way is LISP tradition.
     fn cdr(&self) -> Self {
         if let Cons { cdr, .. } = &*self.0.read() {
-            cdr.clone()
+            *cdr
         } else {
             panic!("cdr of nil")
         }
     }
 
+    /// Check whether a list is empty.
     fn is_nil(&self) -> bool {
         matches!(&*self.0.read(), Nil)
     }
 
+    /// Print the list on one line in LISP format. This will
+    /// loop forever if the list is cyclic.
     fn print(&self) {
-        let mut cur = self.clone();
+        let mut cur = *self;
         let mut sep = "";
+        print!("(");
         while !cur.is_nil() {
             print!("{sep}");
             print!("{}", cur.car());
             cur = cur.cdr();
             sep = " ";
         }
-        println!();
-    }
-}
-
-impl Clone for List<'_> {
-    fn clone(&self) -> Self {
-        List(self.0)
+        println!(")");
     }
 }
 
 fn main() {
+    // Set up an arena with default parameters.
     let params = ArenaParameters::default();
     let mut arena = MyArena::new(params, |mc| MyRoot(GcCell::allocate(mc, vec![])));
 
+    // Create a couple of lists, with the tail of `l2` being
+    // `l1`. Then store them as roots in the arena.
     arena.mutate(|mc, root| {
         let l1 = List::new(mc).cons(mc, 1);
-        let l2 = l1.clone().cons(mc, 2);
+        let l2 = l1.cons(mc, 2);
         root.0.write(mc).push(l1);
         root.0.write(mc).push(l2);
     });
 
+    // Print the two root lists. Yep, they're still there
+    // and look OK.
+    //
+    // Garbage collection may happen before this. Thus, it
+    // is generally better to break up the operations into
+    // small chunks so the GC gets a chance to operate.
     arena.mutate(|_, root| {
         let roots: &Vec<List<'_>> = &root.0.read();
         roots[0].print();
